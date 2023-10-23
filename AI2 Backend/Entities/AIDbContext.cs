508cf897f1b44dad280399374e735b376d97110c
Kstyk <80002380+Kstyk@users.noreply.github.com>
2023-10-23 13:42:40 +0200

    Dodanie DbContextu, utworzenie pierwszego modelu Usera oraz utworzenie pierwszej migracji

---
+++ AI2 Backend/Entities/AIDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AI2_Backend.Entities
+{
+    public class AIDbContext : DbContext
+    {
+        public AIDbContext(DbContextOptions<AIDbContext> options) : base(options)
+        {
+            
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+   
+    }
+}
---
+++ AI2 Backend/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace AI2_Backend.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            
+        }
+    }
+}
