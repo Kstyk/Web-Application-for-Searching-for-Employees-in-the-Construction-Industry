0351ff56f8f7c574221683852aef5b5761647db1
Kstyk <80002380+Kstyk@users.noreply.github.com>
2023-10-23 13:41:32 +0200

    Dodanie połączenia z bazą danych MySql z użyciem DbContext do Program.cs. Zainstalowanie Pomelo.EFCore.MySql

--- AI2 Backend/Program.cs
+++ AI2 Backend/Program.cs
@@ -1,4 +1,15 @@
+using AI2_Backend.Entities;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// database connection
 
 // Add services to the container.
 
@@ -6,6 +17,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+// db Context
+builder.Services.AddDbContextPool<AIDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Default");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+builder.Services.AddHttpContextAccessor();
+
 
 var app = builder.Build();
 
