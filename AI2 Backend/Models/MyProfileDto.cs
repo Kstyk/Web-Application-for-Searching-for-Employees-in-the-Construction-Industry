221a9ddf61017f5a56b783d0d6607f9351e24cd3
Seweryn Drag <dragseweryn@zohomail.eu>
2023-11-22 17:49:33 +0100

    Oddzielne dto do profilu zalogowanego usera

---
+++ AI2 Backend/Models/MyProfileDto.cs
@@ -0,0 +1,21 @@
+﻿using AI2_Backend.Entities;
+using AI2_Backend.Enums;
+
+namespace AI2_Backend.Models
+{
+    public class MyProfileDto
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? AboutMe { get; set; }
+        public string? Education { get; set; }
+        public Voivodeship? Voivodeship { get; set; }
+        public decimal? RequiredPayment { get; set; }
+        public ICollection<QualificationDto> UserQualifications { get; set; }
+        public ICollection<UserExperience> UserExperiences { get; set; } = new List<UserExperience>();
+
+    }
+}
