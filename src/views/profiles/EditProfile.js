75cab92b96c53541706cb0a3160e05d328554b3c
Patryk <patrykchrobak@o2.pl>
2023-12-17 22:06:17 +0100

    edycja i usuwanie profilu

--- src/views/profiles/EditProfile.js
+++ src/views/profiles/EditProfile.js
@@ -1,49 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import {
-    Card,
-    CardContent,
-    Container,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
     FormControlLabel,
+    IconButton,
     Grid,
     Switch,
     TextField,
     Button,
     Typography,
+    Autocomplete,
 } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
+import { useForm, Controller } from 'react-hook-form';
 
 const api = axios.create({
     baseURL: 'http://localhost:5072/api',
-  });
+});
 
-const EditProfile = () => {
-    const [userData, setUserData] = useState({
-        id: 5,
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        province: 'Mazowieckie',
-        earnings: '60,000 PLN',
-        specialties: ['Hydraulik', 'Tynkarz'],
-        experience: {
-            hydraulik: '5 lat',
-            tynkarz: '7 lat',
-        },
-        shortInfo: 'Jestem doświadczonym hydraulikiem i tynkarzem.',
-        school: 'Politechnika Warszawska',
-        isDataVisible: true,
-        isProfileVisible: true,
-    });
+const EditProfile = ({ profile, onClose, onProfileEdited }) => {
+    const [userData, setUserData] = useState(profile);
+    const [qualifications, setQualifications] = useState([]);
+    const {
+        control,
+        handleSubmit,
+        setValue,
+        register,
+        formState: { errors },
+    } = useForm({});
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUserData({
-            ...userData,
-            [name]: value,
-        });
+    const voivodeships = [
+        'dolnośląskie',
+        'kujawskoPomorskie',
+        'lubelskie',
+        'lubuskie',
+        'łódzkie',
+        'małopolskie',
+        'mazowieckie',
+        'opolskie',
+        'podkarpackie',
+        'podlaskie',
+        'pomorskie',
+        'śląskie',
+        'świętokrzyskie',
+        'warmińskoMazurskie',
+        'wielkopolskie',
+        'zachodnioPomorskie',
+    ];
+
+    const fetchQualification = async () => {
+
+        await api.get('/qualifications', {})
+            .then((res) => {
+                setQualifications(res.data);
+            })
+            .catch((err) => {
+                console.error('Error fetching qualification');
+            });
     };
+
+    fetchQualification();
+
+    useEffect(() => {
+        register('firstName', { required: 'To pole jest wymagane' });
+        register('lastName', { required: 'To pole jest wymagane' });
+        register('email');
+        register('aboutMe');
+        register('education');
+        register('voivodeship');
+        register('requiredPayment');
+        register('qualificationsToAdd');
+        register('userExperiences');
+        register('userQualifications');
+    }, [register]);
+
+    useEffect(() => {
+        setValue('firstName', userData.firstName);
+        setValue('lastName', userData.lastName);
+        setValue('email', userData.email);
+        setValue('aboutMe', userData?.aboutMe || null);
+        setValue('education', userData?.education || null);
+        setValue('voivodeship', userData?.voivodeship || null);
+        setValue('requiredPayment', userData?.requiredPayment || null);
+        setValue('qualificationsToAdd', userData?.qualificationsToAdd || []);
+        setValue('userExperiences', userData?.userExperiences || []);
+        setValue('userQualifications', userData?.userQualifications || []);
+    }, [userData, setUserData]);
+
 
     const handleSwitchChange = (event) => {
         const { name, checked } = event.target;
@@ -54,32 +102,50 @@ const EditProfile = () => {
     };
 
     const navigate = useNavigate();
-    const handleSubmit = () => {  // dane w formularzu chyba powinny być przechowywane w innej zmiennej niż userData
+    const onSubmit = async (data) => {
+        console.log(userData);
+        console.log(data);
         const updateUserProfile = async () => {
-        try {
-            const token = localStorage.getItem('token');
+            try {
+                const token = localStorage.getItem('token');
 
-            const response = await api.put('/account/update', userData, {
-            headers: {
-                Authorization: `${token}`,
-            },
-            });
-            navigate('/account/my_profile');
-        } catch (error) {
-            console.error('Error fetching user profile:', error.message);
-        }
-            
+                const response = await api.put('/account/update', data, {
+                    headers: {
+                        Authorization: `${token}`,
+                    },
+                });
+                onProfileEdited(data)
+            } catch (error) {
+                console.error('Error editing user profile:', error.message);
+            }
+
         };
         updateUserProfile();
+        onClose();
+    };
+
+    const getFilteredOptions = (allOptions, selectedOptions) => {
+        const selectedIds = selectedOptions.map((item) => item.id);
+        return allOptions.filter((option) => !selectedIds.includes(option.id));
     };
 
     return (
-        <Container maxWidth="sm">
-            <Card>
-                <CardContent>
-                    <Typography variant="h6" gutterBottom>
-                        Edytuj profil
-                    </Typography>
+        <Dialog open={true} onClose={onClose}>
+            <DialogTitle
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                }}
+            >
+                Dodaj nowe zadanie
+                <IconButton edge="end" color="inherit" onClick={onClose} aria-label="close">
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <DialogContent>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <Grid container spacing={3}>
                         <Grid item xs={12}>
                             <FormControlLabel
@@ -95,43 +161,95 @@ const EditProfile = () => {
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
+                            <Controller
                                 name="firstName"
-                                label="Imię"
-                                variant="outlined"
-                                fullWidth
-                                value={userData.firstName}
-                                onChange={handleInputChange}
+                                control={control}
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        label="Imię"
+                                        variant="outlined"
+                                        fullWidth
+                                    />
+                                )}
                             />
+                            {errors.firstName && (
+                                <Typography variant="caption" color="error">
+                                    {errors.firstName.message}
+                                </Typography>
+                            )}
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
+                            <Controller
                                 name="lastName"
-                                label="Nazwisko"
-                                variant="outlined"
-                                fullWidth
-                                value={userData.lastName}
-                                onChange={handleInputChange}
+                                control={control}
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        label="Nazwisko"
+                                        variant="outlined"
+                                        fullWidth
+                                    />
+                                )}
                             />
+                            {errors.lastName && (
+                                <Typography variant="caption" color="error">
+                                    {errors.lastName.message}
+                                </Typography>
+                            )}
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                name="email"
-                                label="Email"
-                                variant="outlined"
-                                fullWidth
-                                value={userData.email}
-                                onChange={handleInputChange}
+                            <Controller
+                                name="userQualifications"
+                                control={control}
+                                render={({ field }) => (
+                                    <Autocomplete
+                                        {...field}
+                                        className="my-3"
+                                        multiple
+                                        id="tags-outlined"
+                                        options={getFilteredOptions(qualifications, field.value)}
+                                        getOptionLabel={(qualification) => qualification.name}
+                                        filterSelectedOptions
+                                        onChange={(event, newValue) => {
+                                            setValue('userQualifications', newValue);
+                                            setValue('qualificationsToAdd', newValue.map(item => item.id));
+                                        }}
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label="Kwalifikacje"
+                                                placeholder=""
+                                                variant="outlined"
+                                            />
+                                        )}
+                                    />
+                                )}
                             />
+
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                name="province"
-                                label="Województwo"
-                                variant="outlined"
-                                fullWidth
-                                value={userData.province}
-                                onChange={handleInputChange}
+                            <Controller
+                                name="voivodeship"
+                                control={control}
+                                render={({ field }) => (
+                                    <Autocomplete
+                                        {...field}
+                                        options={voivodeships}
+                                        onChange={(event, newValue) => {
+                                            setValue('voivodeship', newValue);
+                                        }}
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label="Województwo"
+                                                variant="outlined"
+                                                fullWidth
+                                                helperText={errors.voivodeship && errors.voivodeship.message}
+                                            />
+                                        )}
+                                    />
+                                )}
                             />
                             <FormControlLabel
                                 control={
@@ -146,13 +264,18 @@ const EditProfile = () => {
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                name="earnings"
-                                label="Zarobki"
-                                variant="outlined"
-                                fullWidth
-                                value={userData.earnings}
-                                onChange={handleInputChange}
+                            <Controller
+                                name="requiredPayment"
+                                control={control}
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        label="Zarobki"
+                                        variant="outlined"
+                                        fullWidth
+                                        type='number'
+                                    />
+                                )}
                             />
                             <FormControlLabel
                                 control={
@@ -167,13 +290,18 @@ const EditProfile = () => {
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                name="shortInfo"
-                                label="Krótki opis"
-                                variant="outlined"
-                                fullWidth
-                                value={userData.shortInfo}
-                                onChange={handleInputChange}
+                            <Controller
+                                name="aboutMe"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        label="Krótki opis"
+                                        variant="outlined"
+                                        fullWidth
+                                    />
+                                )}
                             />
                             <FormControlLabel
                                 control={
@@ -188,13 +316,18 @@ const EditProfile = () => {
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                name="school"
-                                label="Ukończona szkoła"
-                                variant="outlined"
-                                fullWidth
-                                value={userData.school}
-                                onChange={handleInputChange}
+                            <Controller
+                                name="education"
+                                control={control}
+                                defaultValue=""
+                                render={({ field }) => (
+                                    <TextField
+                                        {...field}
+                                        label="Ukończona szkoła"
+                                        variant="outlined"
+                                        fullWidth
+                                    />
+                                )}
                             />
                             <FormControlLabel
                                 control={
@@ -209,7 +342,7 @@ const EditProfile = () => {
                             />
                         </Grid>
                     </Grid>
-                    <Grid container justifyContent="flex-end">
+                    {/* <Grid container justifyContent="flex-end">
                         <Grid item>
                             <Link to={`/profile/${userData.id}`}>
                                 <Button
@@ -221,10 +354,18 @@ const EditProfile = () => {
                                 </Button>
                             </Link>
                         </Grid>
-                    </Grid>
-                </CardContent>
-            </Card>
-        </Container>
+                    </Grid> */}
+                    <DialogActions>
+                        <Button onClick={onClose} size="medium" variant="outlined" color="primary">
+                            Anuluj
+                        </Button>
+                        <Button type="submit" size="medium" variant="contained" color="primary">
+                            Edytuj profil
+                        </Button>
+                    </DialogActions>
+                </form>
+            </DialogContent>
+        </Dialog>
     );
 };
 
