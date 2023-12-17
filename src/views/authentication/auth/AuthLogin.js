6b3ea5e5aa7d816c3c91e652add635d37aee4959
Patryk <patrykchrobak@o2.pl>
2023-12-17 21:31:08 +0100

    podpięcie logowania i rejestracji do backendu oraz dodanie walidacji

--- src/views/authentication/auth/AuthLogin.js
+++ src/views/authentication/auth/AuthLogin.js
@@ -4,69 +4,106 @@ import {
   Typography,
   Button,
   Stack,
+  TextField
 } from '@mui/material';
 
 import CustomTextField from '../../../components/forms/theme-elements/CustomTextField';
 
 import { useAuth } from '../../../contexts/AuthContext'
+import { useForm } from 'react-hook-form';
 
 const AuthLogin = ({ subtitle }) => {
-    const [credentials, setCredentials] = useState({ email: '', password: '' });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const { login } = useAuth();
+  const { login } = useAuth();
+  const [loginError, setLoginError] = useState(null);
 
-    const handleFormSubmit = (e) => {
-      e.preventDefault();
+  const onSubmit = async (data) => {
+    try {
+      await login(data);
+    } catch (error) {
+      setLoginError(error.response.data);
+    }
+  };
 
-      login(credentials);
-    };
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box>
+          <Typography
+            variant="subtitle1"
+            fontWeight={600}
+            component="label"
+            htmlFor="email"
+            mb="5px"
+          >
+            Email
+          </Typography>
+          <TextField
+            id="email"
+            name="email"
+            variant="outlined"
+            fullWidth
+            // type='email'
+            {...register('email', {
+              required: 'To pole jest wymagane',
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: 'Wprowadzona wartość powinna być adresem email',
+              },
+            })}
+          />
+          {errors.email && (
+            <Typography variant="caption" color="error">
+              {errors.email.message}
+            </Typography>
+          )}
+        </Box>
+        <Box mt="25px">
+          <Typography
+            variant="subtitle1"
+            fontWeight={600}
+            component="label"
+            htmlFor="password"
+            mb="5px"
+          >
+            Hasło
+          </Typography>
+          <TextField
+            id="password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            {...register('password', { required: 'To pole jest wymagane' })}
+          />
+          {errors.password && (
+            <Typography variant="caption" color="error">
+              {errors.password.message}
+            </Typography>
+          )}
+        </Box>
 
-    return (
-      <>
-        <form onSubmit={ handleFormSubmit }>
-          <Stack>
-            <Box>
-              <Typography variant="subtitle1" fontWeight={600} component="label" htmlFor="email" mb="5px">
-                Email
-              </Typography>
-              <CustomTextField
-                id="email"
-                variant="outlined"
-                fullWidth
-                value={ credentials.email }
-                onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}
+        {loginError && (
+          <div className="text-center">
+            <Typography variant="caption" color="error">
+              {loginError}
+            </Typography>
+          </div>
+        )}
 
-              />
-            </Box>
-            <Box className="mt-6">
-              <Typography variant="subtitle1" fontWeight={600} component="label" htmlFor="password" mb="5px">
-                Hasło
-              </Typography>
-              <CustomTextField
-                id="password"
-                type="password"
-                variant="outlined"
-                fullWidth
-                value={ credentials.password } 
-                onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-              />
-            </Box>
-          </Stack>
-          <Box className="mt-8">
-            <Button
-              color="primary"
-              variant="contained"
-              size="large"
-              fullWidth
-              type="submit"
-            >
-              Zaloguj
-            </Button>
-          </Box>
-        </form>
-        {subtitle}
-      </>
-    );
+        <Box mt="30px" mb="20px">
+          <Button color="primary" variant="contained" size="large" fullWidth type="submit">
+            Zaloguj
+          </Button>
+        </Box>
+      </form>
+      {subtitle}
+    </>
+  );
 }
 
 export default AuthLogin;
